d55cba9f5b522303cf4c9194984de738bf0df91e
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-14 20:56:04 +0100

    setup bars

--- ScollBars/ContentView.swift
+++ ScollBars/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var bars: [Bar]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        ScrollView {
+
+            VStack(spacing: 30){
+
+                ForEach(bars, id: \.self) { bar in
+
+                    RoundedRectangle(cornerRadius: 40)
+                        .fill(bar.color)
+                        .frame(width: 300, height: 40)
+                        .onGeometryChange(for: CGRect.self) { proxy in
+                            proxy.frame(in: .global)
+                        } action: { newValue in
+                            
+                        }
+
+
+
+                }
+            }
         }
         .padding()
     }
 }
 
+//#Preview {
+//    ContentView(colors: )
+//}
+
 #Preview {
-    ContentView()
+    ContentView(bars: [Bar(color: .blue),Bar(color: .red),Bar(color: .brown),Bar(color: .white)])
 }
--- ScollBars/ScollBarsApp.swift
+++ ScollBars/ScollBarsApp.swift
@@ -11,7 +11,28 @@ import SwiftUI
 struct ScollBarsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+
+            ContentView(bars: getBars())
+                .preferredColorScheme(.dark)
         }
     }
+
+    func getBars() -> [Bar] {
+        (0..<100).map { _ in
+            Color(
+                red: Double.random(in: 0...1),
+                green: .random(in: 0...1),
+                blue: .random(in: 0...1)
+            )
+        }
+        .map { color in
+            Bar(color: color, rect: .zero)
+        }
+    }
+}
+
+
+struct Bar: Hashable {
+    var color: Color
+    var rect: CGRect = .zero
 }
