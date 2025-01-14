daa2db42a0a8a2b0904c3a4f2a47091069937284
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-14 20:04:03 +0100

    Initial Commit

---
+++ ScollBars/ScollBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScollBarsApp.swift
+//  ScollBars
+//
+//  Created by Mohamed Alwakil on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct ScollBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
